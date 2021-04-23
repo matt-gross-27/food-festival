94396865e56561c941205ea55524bd383349c116
Matt Gross <mbgross111@gmail.com>
2021-04-22 22:25:05 -0700

    pwa dcan be installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,49 +1,68 @@
+const webpack = require("webpack");
 const path = require("path");
-const webpack = require("webpack")
-const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPlugin;
+const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+const WebpackPwaManifest = require("webpack-pwa-manifest");
 
-module.exports = {
+
+const config = {
   entry: {
-    app: "./assets/js/script.js",
-    events: "./assets/js/events.js",
-    schedule: "./assets/js/schedule.js",
-    tickets: "./assets/js/tickets.js"
+    app: './assets/js/script.js',
+    events: './assets/js/events.js',
+    schedule: './assets/js/schedule.js',
+    tickets: './assets/js/tickets.js'
   },
   output: {
-    filename: "[name].bundle.js",
-    path: path.join(__dirname + "/dist")
+    filename: '[name].bundle.js',
+    path: __dirname + '/dist'
   },
-  module: {
-    rules: [
-      {
-        test: /\.(jpg|png|gif|svg)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name (file) {
-                return "[path][name].[ext]"
-              },
-              publicPath: function(url) {
-                return url.replace("../", "/assets/")
-              }
-            }
-          },
-          {
-            loader: "image-webpack-loader"
-          }
-        ]
-      }
-    ]
-  },
+  // module: {
+  //   rules: [
+  //     {
+  //       test: /\.(png|jpe?g|gif)$/i,
+  //       use: [
+  //         {
+  //           loader: 'file-loader',
+  //           options: {
+  //             name(file) {
+  //               return '[path][name].[ext]'
+  //             },
+  //             publicPath: function (url) {
+  //               return url.replace('../', '/assets/')
+  //             },
+  //           }
+  //         },
+  //         {
+  //           loader: 'image-webpack-loader',
+  //         },
+  //       ],
+  //     },
+  //   ],
+  // },
   plugins: [
     new webpack.ProvidePlugin({
       $: "jquery",
       jQuery: "jquery"
     }),
     new BundleAnalyzerPlugin({
-      analyzerMode: "static", // report -> dist/report.html
+      analyzerMode: "static"
+    }),
+    new WebpackPwaManifest({
+      name: "Food Event",
+      short_name: "Foodies",
+      description: "An app that allows you to view upcoming food events.",
+      start_url: "../index.html",
+      background_color: "#01579b",
+      theme_color: "#ffffff",
+      fingerprints: false,
+      inject: false,
+      icons: [{
+        src: path.resolve("assets/img/icons/icon-512x512.png"),
+        sizes: [96, 128, 192, 256, 384, 512],
+        destination: path.join("assets", "icons")
+      }]
     })
   ],
   mode: "development"
 };
+
+module.exports = config;
